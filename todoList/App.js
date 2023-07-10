302a97bbb1e417c6d6401d88cc4a45936452c975
HWANBINYOO <dngh0825@naver.com>
2023-07-10 15:34:53 +0900

    Add : Paint To Dos

--- todoList/App.js
+++ todoList/App.js
@@ -1,5 +1,5 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import {
   StyleSheet,
   Text,
@@ -9,24 +9,23 @@ import {
   TouchableWithoutFeedback,
   Pressable,
   TextInput,
-} from "react-native";
-import { theme } from "./color";
+  ScrollView,
+} from 'react-native';
+import { theme } from './color';
 export default function App() {
   const [working, setWorking] = useState(true);
-  const [text, setText] = useState("");
+  const [text, setText] = useState('');
   const [toDos, setToDos] = useState({});
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
   const onChangeText = (payload) => setText(payload);
   const addToDo = () => {
-    if (text === "") {
+    if (text === '') {
       return;
     }
-    const newToDos = Object.assign({}, toDos, {
-      [Date.now()]: { text, work: working },
-    });
+    const newToDos = { ...toDos, [Date.now()]: { text, work: working } };
     setToDos(newToDos);
-    setText("");
+    setText('');
   };
   return (
     <View style={styles.container}>
@@ -34,14 +33,14 @@ export default function App() {
       <View style={styles.header}>
         <TouchableOpacity onPress={work}>
           <Text
-            style={{ ...styles.btnText, color: working ? "white" : theme.grey }}
+            style={{ ...styles.btnText, color: working ? 'white' : theme.grey }}
           >
             Work
           </Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={travel}>
           <Text
-            style={{ ...styles.btnText, color: working ? theme.grey : "white" }}
+            style={{ ...styles.btnText, color: working ? theme.grey : 'white' }}
           >
             Travel
           </Text>
@@ -53,10 +52,17 @@ export default function App() {
           onChangeText={onChangeText}
           returnKeyType="done"
           value={text}
-          placeholder={working ? "Add a To Do" : "Where do you want to go?"}
+          placeholder={working ? 'Add a To Do' : 'Where do you want to go?'}
           style={styles.input}
         />
       </View>
+      <ScrollView>
+        {Object.keys(toDos).map((key) => (
+          <View style={styles.toDo} key={key}>
+            <Text style={styles.toDoText}>{toDos[key].text}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -68,20 +74,32 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
   },
   header: {
-    justifyContent: "space-between",
-    flexDirection: "row",
+    justifyContent: 'space-between',
+    flexDirection: 'row',
     paddingTop: 100,
   },
   btnText: {
     fontSize: 38,
-    fontWeight: "600",
+    fontWeight: '600',
   },
   input: {
-    backgroundColor: "white",
+    backgroundColor: 'white',
     paddingVertical: 15,
     paddingHorizontal: 20,
     borderRadius: 20,
-    marginTop: 20,
+    marginVertical: 20,
     fontSize: 18,
+  },
+  toDo: {
+    backgroundColor: theme.grey,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 15,
+  },
+  toDoText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '500',
   },
 });
